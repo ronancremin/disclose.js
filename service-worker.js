2e077b292beed9d6418f441a4f0c35305011cb45
Ronan Cremin <rcremin@afilias.info>
2015-11-26 17:06:28 +0000

    Debugging added, indentation fixed

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,21 @@
-var bytesSinceInit = 0;
-var requestsSinceInit = 0;
-var activeRequests = {};
-var completedRequests = {};
+var bytesSinceInit = 0,
+    requestsSinceInit = 0,
+    activeRequests = {},
+    completedRequests = {};
 
 var maxBandwidth = 0,
     minBandwidth = 0,
     currentBandwidth = 0;
 
+var datachunks = {};
+var foo = '';
+
 
 self.clients.matchAll().then(function(clients) {
-  clients.forEach(function(client) {
-    console.log(client);
-    //client.postMessage('The service worker just started up.');
-  });
+    clients.forEach(function(client) {
+        console.log(client);
+        //client.postMessage('The service worker just started up.');
+    });
 });
 
 
@@ -22,11 +25,11 @@ self.addEventListener('activate', function(event) {
 
 
 self.addEventListener('fetch', function(event) {
-    var startTime, endTime;
-    var url = event.request.url;
-    startTime = Date.now();
+    var startTime = Date.now(),
+        url = event.request.url;
     console.log('Handling fetch event for', url);
     activeRequests[url] = {bytes: 0, startTime: Date.now(), elapsedTime: 0};
+    datachunks[url] = {bytes: ''};
     requestsSinceInit++;
     updateBrowser();
 
@@ -34,39 +37,48 @@ self.addEventListener('fetch', function(event) {
         .then(res => consume(res.body.getReader(), 0, url))
         //.then(() => console.log('consume finished for ' + event.request.url))
         .then(() => {
-            endTime = Date.now(); 
-            var delta = endTime - startTime; 
+            var delta = Date.now() - startTime; 
             console.log('consume finished for ' + url + ', elapsed:', delta);
 
             completedRequests[url] = activeRequests[url];
             completedRequests[url].KB = (completedRequests[url].bytes/1024).toFixed(1) + 'KB';
-
             delete activeRequests[url];
 
-            // log the completed requests if none outstanding
+            // display the completed requests if none outstanding
             if (Object.keys(activeRequests).length == 0) {
                 console.log('No outstanding requests, completed requests are:');
                 console.table(completedRequests);
+                console.table(datachunks);
+                console.log('bytes since init', bytesSinceInit);
+                console.log(foo);
+                var tmp = 0;
+                for (var key in completedRequests) {
+                    tmp += completedRequests[key].bytes;
+                }
+                console.log('added up bytes:', tmp);
+
             }
         })
-	.then(() => updateBrowser())
-        .catch((e) => console.error("something went wrong with the fetch() request", e));
+    .then(() => updateBrowser())
+    .catch((e) => console.error("something went wrong with the fetch() request", e));
 
 });
 
 
-// handle reset message from browser
+// handle messages from browser
 self.addEventListener('message', function(event) {
     console.log('SW got message', event.data);
     if (event.data.command == 'reset') {
-	self.bytesSinceInit = 0;
-	self.requestsSinceInit = 0;
-	self.maxBandwidth = 0;
-	self.minBandwidth = 0;
-	self.currentBandwidth = 0;
-	updateBrowser();
+        self.bytesSinceInit = 0;
+        self.requestsSinceInit = 0;
+        self.maxBandwidth = 0;
+        self.minBandwidth = 0;
+        self.currentBandwidth = 0;
+        self.activeRequests = {};
+        self.completedRequests = {};
+        updateBrowser();
     } else {
-    	console.log('Ignoring command');
+        console.log('Ignoring command');
     }
 })
 
@@ -74,7 +86,7 @@ self.addEventListener('message', function(event) {
 // consume bytes from reader and keep track
 function consume(reader, total, url) {
     //console.log('consume running for', url);
-    total = typeof total !== 'undefined' ? total : 0;
+    var total = typeof total !== 'undefined' ? total : 0;
     calculateBandwidth();
     return reader.read().then(function(result) {
         if (result.done) {
@@ -83,6 +95,11 @@ function consume(reader, total, url) {
             return;
         }
         total += result.value.byteLength;
+        //console.warn(result.value.byteLength, bytesSinceInit);
+
+
+        datachunks[url].bytes += result.value.byteLength + ' ';
+        foo += result.value.byteLength + ' + ';
 
         var activeRequest = activeRequests[url];
         activeRequest.bytes = total;
@@ -100,11 +117,11 @@ function consume(reader, total, url) {
 function updateBrowser() {
     calculateBandwidth();
     sendMessageToBrowser({
-	bytesSinceInit: bytesSinceInit, 
-	requestsSinceInit: requestsSinceInit, 
-	inFlightRequests: Object.keys(activeRequests).length,
-	currentBandwidth: currentBandwidth,
-	maxBandwidth: maxBandwidth
+        bytesSinceInit: bytesSinceInit,
+        requestsSinceInit: requestsSinceInit,
+        inFlightRequests: Object.keys(activeRequests).length,
+        currentBandwidth: currentBandwidth,
+        maxBandwidth: maxBandwidth
     });
 }
 
@@ -119,11 +136,11 @@ function sendMessageToBrowser(message) {
 }
 
 
+// Loops through in-flight requests' current status to estimate bandwidth
 function calculateBandwidth() {
-
     if (Object.keys(activeRequests).length == 0) {
-    	currentBandwidth = 0;
-    	return;
+        currentBandwidth = 0;
+        return;
     }
 
     var tmpBandwidth = 0;
@@ -132,10 +149,10 @@ function calculateBandwidth() {
         if (activeRequests.hasOwnProperty(url)) {
             //console.log(url, activeRequests[url].elapsedTime, activeRequests[url].bytes);
             if (activeRequests[url].elapsedTime) { // check for non-zero
-                currentBandwidth += activeRequests[url].bytes / activeRequests[url].elapsedTime * 1000;
+                tmpBandwidth += activeRequests[url].bytes / activeRequests[url].elapsedTime * 1000;
             }
         }
-    } 
+    }
     if (tmpBandwidth != 0) currentBandwidth = tmpBandwidth;
     maxBandwidth = currentBandwidth > maxBandwidth ? currentBandwidth : maxBandwidth;
     //console.log('Current bandwidth:', (currentBandwidth/1024).toFixed(1), 'KB/s, max bandwidth:', (maxBandwidth/1024).toFixed(1));
